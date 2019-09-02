412c8a1d3e90e07530247e0eea7778be8f21d7bb
James Pace <jpace121@gmail.com>
2019-09-02 19:28:53 -0400

    Add ros2 from scratch workspace. Switch names with dashing install.

--- build-ros2.sh
+++ build-ros2.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-# Build container for running ros2
+username=$(whoami)
+
+# Build container for running ros2 from source.
 
 container=$(buildah from localhost/jwp-build-latest)
 
-echo "==> Dependencies."
-buildah run --net host $container -- apt install -y curl gnupg2 lsb-release
 echo "==> Apt key"
+buildah run --net host $container -- apt install -y curl gnupg2 lsb-release
 buildah run --net host $container -- bash -c 'curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -'
 buildah run --net host $container -- bash -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
-buildah run --net host $container -- apt update
-echo "==> Install."
-buildah run --net host $container -- apt install -y ros-dashing-desktop
-buildah run --net host $container -- apt install -y python3-colcon-common-extensions
+
+echo "==> Copy and run Script."
+buildah copy --chown $username:$username $container ./ros2/build-from-source.bash /home/jimmy/build-from-source.bash
+buildah run --user $username $container -- bash -c 'chmod +x /home/jimmy/build-from-source.bash'
+buildah run --net host --user $username $container -- bash -c '/home/jimmy/build-from-source.bash'
 
 echo "==> Save image."
-buildah commit $container jwp-ros2-latest
-buildah rm $container
+buildah commit --squash --rm $container jwp-ros2-latest
 
 #echo "===> Send to Docker."
 #buildah push jwp-ros2-latest docker-daemon:jwp-ros2:latest
+
---
+++ ros2/build-from-source.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Build ROS 2 Eloquent from scratch.
+sudo apt update && sudo apt install -y \
+  build-essential \
+  cmake \
+  git \
+  python3-colcon-common-extensions \
+  python3-pip \
+  python-rosdep \
+  python3-vcstool \
+  wget
+# install some pip packages needed for testing
+python3 -m pip install -U \
+  argcomplete \
+  flake8 \
+  flake8-blind-except \
+  flake8-builtins \
+  flake8-class-newline \
+  flake8-comprehensions \
+  flake8-deprecated \
+  flake8-docstrings \
+  flake8-import-order \
+  flake8-quotes \
+  pytest-repeat \
+  pytest-rerunfailures \
+  pytest \
+  pytest-cov \
+  pytest-runner \
+  setuptools
+# install Fast-RTPS dependencies
+sudo apt install --no-install-recommends -y \
+  libasio-dev \
+  libtinyxml2-dev
+
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws
+wget https://raw.githubusercontent.com/ros2/ros2/master/ros2.repos
+vcs import src < ros2.repos
+
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro eloquent -y --skip-keys "console_bridge fastcdr fastrtps libopensplice67 libopensplice69 rti-connext-dds-5.3.1 urdfdom_headers"
+
+cd ~/ros2_ws/
+colcon build --symlink-install
