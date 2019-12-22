eb8e9a8aea78840d46b35f6ff1ef4a831871313f
James Pace <jpace121@gmail.com>
2019-12-22 15:00:25 -0500

    Save version to file. Print in command.

--- setup.py
+++ setup.py
@@ -1,19 +1,46 @@
 from setuptools import setup
+import subprocess
 import sys
+import os
+
+package_name='dev_contain'
+version = '0.0.1'
+
+setup_file_path = os.path.dirname(os.path.abspath(__file__))
 
 if sys.version_info < (3,):
     print('dev_contain only supports Python 3.')
     sys.exit(1)
 
+def set_version_file():
+    # Get git version if we can.
+    git_version = 'Unknown'
+    try:
+        check_result = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        git_version = check_result.stdout.read().decode('ascii').strip()
+    except:
+        pass
+
+    # Save git version and version variable to a file.
+    with open(os.path.join(setup_file_path, package_name, 'version.yaml'), 'w+') as f:
+        f.write('version: {}\n'.format(version))
+        f.write('hash: {}\n'.format(git_version))
+         
+set_version_file()
+
 setup(name='dev_contain',
-      version='0.0.0',
+      version=version,
       description='CLI script to build and use containers for development leveraging podman.',
       author='James Pace',
       author_email='jpace121@gmail.com',
       url='dev_contain.local',
       license='Apache 2.0',
-      packages=['dev_contain'],
+      packages=[package_name],
+      setup_requires=['wheel'],
       install_requires=['jinja2', 'pyyaml'],
+      package_data={
+        '': ['version.yaml'],
+      },
       entry_points = {
         'console_scripts': ['dev_contain=dev_contain.dev_contain:main'],
       },
