52a945396cdbfe35fbc6dfa691234d422e2d1fb5
James Pace <jpace121@gmail.com>
2020-01-22 19:10:42 -0500

    List all containers and images always.

--- dev_contain/list.py
+++ dev_contain/list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 #
 # Copyright 2019 James Pace
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,8 +20,7 @@ import subprocess
 import dev_contain.common as common
 
 def list_(in_args):
-    parser = argparse.ArgumentParser(prog=sys.argv[0]+' list', description='List known containers or images.')
-    parser.add_argument('type', choices=['images', 'containers'], help='Whether to list containers or images.')
+    parser = argparse.ArgumentParser(prog=sys.argv[0]+' list', description='List known containers and images.')
     parser.add_argument('--all', '-a', action='store_true', help='Also list containers/images not labeled by dev_contain.')
     args = parser.parse_args(in_args)
 
@@ -33,21 +32,24 @@ def list_(in_args):
 
     # Note that python format strings collide with the syntax for the format
     # commands.
-    command = ''
-    if args.type == 'images':
-        command = manager + ' images '\
-                    + filter_text + \
-                    '--filter dangling=false '\
-                    '--format "{{.Repository}}"'
-    elif args.type == 'containers':
-        command = manager + ' ps -a '\
-                  + filter_text + \
-                  '--format "{{.Names}}    {{.Status}}    {{.Image}}"'
-
+    command = manager + ' images '\
+                + filter_text + \
+                '--filter dangling=false '\
+                '--format "{{.Repository}}"'
     output = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+    print('Images: ')
     for line in output.stdout.splitlines():
-        print(line.decode())
+        print('\t' + line.decode())
+
+    command = manager + ' ps -a '\
+                + filter_text + \
+                '--format "{{.Names}}    {{.Status}}    {{.Image}}"'
+    output = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+    print('Containers: ')
+    for line in output.stdout.splitlines():
+        print('\t' + line.decode())
+
 
 if __name__ == '__main__':
     list_(sys.argv[1:])
-    
+
